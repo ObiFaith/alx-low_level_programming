ffb4370fa956da87ed2593a1dd99a1157ab8f577
ObiFaith <obifth@gmail.com>
2022-12-09 15:29:33 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Print using putchar()'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch[27] = "abcdefghijklmnopqrstuvwxyz";
+
+	char ch1[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	int a = 0;
+
+	while (a < 26)
+	{
+		putchar(ch[a]);
+		a++;
+	}
+	while (a < 26)
+	{
+		putchar(ch1[a]);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
