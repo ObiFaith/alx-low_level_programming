493c5aa4b780b0ddb0a11b7dc6fdfc6c0db48212
ObiFaith <obifth@gmail.com>
2022-12-09 16:17:49 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Print Base 10 Numbers'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%d\n", a);
+		a++
+	}
+	return (0);
+}
