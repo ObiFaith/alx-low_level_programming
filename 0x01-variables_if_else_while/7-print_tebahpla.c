61cde56359476b757f01f492d619abea428499aa
ObiFaith <obifth@gmail.com>
2022-12-09 16:52:59 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Base 10 in Reverse order'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch1[27] = "zyxwvutsrqponmlkjihgfedcba";
+	int a = 0;
+
+	while (a < 26)
+	{
+		putchar(ch1[0]);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
