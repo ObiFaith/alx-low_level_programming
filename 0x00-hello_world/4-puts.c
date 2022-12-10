e4322c9cb48a6fabc3a2b8fbe1d900358a050cbf
ObiFaith <obifth@gmail.com>
2022-12-10 04:05:49 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
-#include <stdio.h>                                
-int main(void){                                   puts("\"Programming is like building a multilingual puzzle\n");                                     
-return (0);
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Puts to print output'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
