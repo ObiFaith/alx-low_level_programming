6e6020024c6c1eca409626239263c755846bcd87
ObiFaith <obifth@gmail.com>
2022-12-11 10:36:11 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * Description: 'Using stderr'
  * Return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
-	puts(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
