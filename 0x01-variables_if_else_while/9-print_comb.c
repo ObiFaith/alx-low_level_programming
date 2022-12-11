e917dfe9e143dca5724a2328782ed7647387d2f1
ObiFaith <obifth@gmail.com>
2022-12-11 06:42:00 -0800

    Tast 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Print numbers using putchar'
+ * Return: Always 0 (Success)
+ */
+
+inf main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		putchar(a + '0');
+		putchar(', ');
+		a++;
+	return (0);
+}
