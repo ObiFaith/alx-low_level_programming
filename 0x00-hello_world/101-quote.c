e189e043cc8debdd16019a2da7262ea5a5e30f25
ObiFaith <obifth@gmail.com>
2022-12-11 07:32:08 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,8 @@
  * Description: 'Using stderr'
  * Result: Always 0 (Success)
  */
-
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (0);
 }
