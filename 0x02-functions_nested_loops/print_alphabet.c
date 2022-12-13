78ed57a9d022592e2056ba5fee30dd755a3083e6
ObiFaith <obifth@gmail.com>
2022-12-13 14:13:12 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - A function to prints all letters of the alphabets
+ */
+
+void print_alphabet(void)
+{
+	char alphabets[27] = "abcdefghijklmnopqrstuvwxyz";
+	int a = 0;
+
+	while (a < 26)
+	{
+		_putchar(alphabets[a] + '0');
+		a++;
+	}
+}
