76816d058250bd9576e19589e76eca46ccf9a65f
ObiFaith <obifth@gmail.com>
2022-12-13 18:40:16 -0800

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * times_table - Time tables from 0*[0-9]
+ *
+ * Result: Always 0 (Success)
+ */
+
+void times_table(void)
+{
+	int a = 0;
+
+	int b = 0;
+
+	int n = 10;
+
+	while (a < n)
+	{
+		while (b < n)
+		{
+			printf("%d", a * b);
+			if (!(b == n - 1))
+				printf(", ");
+			b++;
+		}
+		printf("\n");
+		b = 0;
+		a++;
+	}
+}
+
