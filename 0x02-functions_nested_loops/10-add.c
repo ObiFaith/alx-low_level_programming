121f2417b4c5bede2fb70e9b310d58e9e7ef5fce
ObiFaith <obifth@gmail.com>
2022-12-13 18:57:35 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Sum of two nos
+ * @a: Int parameter a
+ * @b: Int parameter b
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
