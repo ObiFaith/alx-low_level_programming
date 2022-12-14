adeeeb57028067d094444b4451cd06f7a1c3ead2
ObiFaith <obifth@gmail.com>
2022-12-13 18:07:28 -0800

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
  * print_last_digit - Print the last digits
- * @num - the digits
+ * @num: the digits
+ *
+ * Return: Always 0 (Success)
  */
 
 int print_last_digit(int num)
