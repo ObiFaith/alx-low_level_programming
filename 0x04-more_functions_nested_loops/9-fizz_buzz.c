d7bb56b0373014d423293a07c5a8d5baeea553b3
ObiFaith <obifth@gmail.com>
2022-12-15 20:00:42 -0800

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: C program for FizzBuzz
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 100;
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (n % 3 == 0)
+			printf("Fizz ");
+		else if (n % 5 == 0)
+			printf("Buzz ");
+		else if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d", n);
+	}
+	return (0);
+}
