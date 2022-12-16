35257fb39116f8e9f441afe6caf5459ed6c65b8c
ObiFaith <obifth@gmail.com>
2022-12-15 16:31:15 -0800

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'Positive_Negative()'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	if (i == 0)
+		printf("0");
+	else
+		positive_or_negative(i);
+
+	return (0);
+}
