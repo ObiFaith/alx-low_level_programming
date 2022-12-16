46386f286bc518d41ecac1ea5894a2e155534f2b
ObiFaith <obifth@gmail.com>
2022-12-15 16:53:57 -0800

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - Function to Multiply to Integers
+ * @a: First Integer Value
+ * @b: Second Integer Value
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
