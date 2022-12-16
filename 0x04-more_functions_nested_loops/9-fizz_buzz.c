d56fc9011d4f28c5835f3d8e05724c47f6d7886d
ObiFaith <obifth@gmail.com>
2022-12-15 20:20:35 -0800

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,16 +13,16 @@ int main(void)
 
 	for (i = 1; i <= n; i++)
 	{
-		if (n % 3 == 0)
+		if (i % 3 == 0)
 			printf(" Fizz");
-		else if (n % 5 == 0)
+		else if (i % 5 == 0)
 			printf(" Buzz");
-		else if (n % 3 == 0 && n % 5 == 0)
+		else if (i % 3 == 0 && i % 5 == 0)
 			printf(" FizzBuzz");
-		else if (n == 1)
-			printf("%d", n);
+		else if (i == 1)
+			printf("%d", i);
 		else
-			printf(" %d", n);
+			printf(" %d", i);
 	}
 	return (0);
 }
