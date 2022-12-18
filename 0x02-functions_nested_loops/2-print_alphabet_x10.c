8d506ce9213e30e9a35c2a785ba26150330f2a3d
ObiFaith <obifth@gmail.com>
2022-12-18 09:47:15 -0800

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,22 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - A function to prints all letters of the alphabets 10 times
+ * print_alphabet_x10 - A function to prints tge alphabets x10
  */
 
 void print_alphabet_x10(void)
 {
+	char ch[] = "abcdefghijklmnopqrstuvwxyz";
 	int a = 0;
+	int b = 0;
 
 	while (a < 10)
 	{
-		char ch = 'a';
-
-		while (ch <= 'z')
+		while (b < 26)
 		{
-			_putchar(ch);
-			ch++;
+			_putchar(ch[b]);
+			b++;
 		}
+		b = 0;
 		a++;
 		_putchar('\n');
 	}
