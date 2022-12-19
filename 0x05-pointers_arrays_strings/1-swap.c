bafd282096c9d3882e496570f236cdfd14b67b14
ObiFaith <obifth@gmail.com>
2022-12-19 03:28:18 -0800

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - Function to swap the value of two pointers
+ * @a: Pointer parameter1
+ * @b: Pointer parameter2
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
