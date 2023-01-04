b84ee3f5c1db6875ea02e5b0fc4d16a7c5494032
ObiFaith <obifth@gmail.com>
2023-01-04 01:59:51 -0800

    Program on 5-more_numbers task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,20 +6,16 @@
 
 void most_numbers(void)
 {
-	int i = 0;
-	int j = 0;
+	int i, j;
 
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		while (j < 15)
+		for (j = 0; j < 15; j++)
 		{
 			if (j > 9)
 				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
-			j++;
+			_putchar(j % 10 + '0');
 		}
-		j = 0;
-		i++;
 		_putchar('\n');
 	}
 }
