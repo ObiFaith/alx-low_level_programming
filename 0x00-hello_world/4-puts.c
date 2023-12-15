71d0a8fa74ed9e941cba6263b7f7769236750019
Obi Faith <obifth@gmail.com>
2023-12-14 16:48:02 -0800

    Completed Task 0, 3 and 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Write my first c program
+ *
+ * Return: Access 0 (void)
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Write my first c program
+ *
+ * Return: Access 0 (void)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
