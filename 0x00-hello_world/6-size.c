c29fc4a7cd7078b1b963501a8e073502f9237cce
Obi Faith <obifth@gmail.com>
2023-12-14 17:31:07 -0800

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Write my first c program
+ *
+ * Return: Access 0 (void)
+*/
+
+int main(void)
+{
+	printf("Size of a char: %d bytes(s)", sizeof(char));
+	printf("Size of an int: %d bytes(s)", sizeof(int));
+	printf("Size of a long int: %d bytes(s)", sizeof(long));
+	printf("Size of float: % bytes(s)", sizeof(float));
+
+	return (0);
+}
