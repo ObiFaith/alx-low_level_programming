ed0b101471fb12187917c042b4958e7e59e30c74
Obi Faith <obifth@gmail.com>
2023-12-16 07:34:18 -0800

    Modified files to meet betty coding style

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,11 @@
 int main(void)
 {
 	int a;
-	
+
 	for (a = 0; a < 10; a++)
-		putchar(a+'0');
+		putchar(a + '0');
 	for (a = 97; a < 103; a++)
+		putchar(a + '0');
 	putchar('\n');
 
 	return (0);
