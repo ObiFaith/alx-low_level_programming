c6610dae4430c62e2a7168149999b4b724ca37aa
Obi Faith <obifth@gmail.com>
2023-12-19 12:16:20 -0800

    Task 0 & 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ * Return: 0 if successful
+ */
+
+void print_alphabet(void)
+{
+	int alp;
+
+	for (alp = 97; alp < 123; alp++)
+		putchar(alp + '0');
+	return (0);
+}
