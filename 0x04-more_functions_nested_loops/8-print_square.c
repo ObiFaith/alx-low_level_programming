ac372ff5e7d0bc499880cc4805b92bd8628c6859
Obi Faith <obifth@gmail.com>
2023-12-21 13:09:13 -0800

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,7 +9,13 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 1; i <= size; i++)
-		for (j = 0; j < i; j++)
-			_putchar('#');
-
+	if (size > 0)
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	else
+		_putchar('\n');
+}
