08cdc00e328ee778f7608d3205ccdeffd006379d
Obi Faith <obifth@gmail.com>
2024-01-02 19:16:46 -0800

    Added conditions in both 6-puts and 7-put-half files

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,7 +9,8 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i]; i += 2)
-		putchar(str[i]);
+	for (i = 0; str[i]; i++)
+		if (i % 2 == 0)
+			putchar(str[i]);
 	putchar('\n');
 }
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,10 @@ void puts_half(char *str)
 {
 	int i, half_len;
 
-	half_len = (strlen(str) - 1) / 2;
+	if (strlen(str) % 2 != 0)
+		half_len = (strlen(str) - 1) / 2;
+	else
+		half_len = strlen(str) / 2;
 
 	for (i = half_len; str[i]; i++)
 		putchar(str[i]);
