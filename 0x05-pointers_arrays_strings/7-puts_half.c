df9fcc5486d04f05c877a3d2aca912062d9b178c
Obi Faith <obifth@gmail.com>
2024-01-02 20:51:28 -0800

    Updated puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,11 +10,11 @@ void puts_half(char *str)
 	int i, half_len;
 
 	if (strlen(str) % 2 != 0)
-		half_len = (strlen(str) - 1) / 2;
+		half_len = ((strlen(str) - 1) / 2) + 1;
 	else
 		half_len = strlen(str) / 2;
 
-	for (i = half_len - 1; str[i]; i++)
+	for (i = half_len; str[i]; i++)
 		putchar(str[i]);
 	putchar('\n');
 }
