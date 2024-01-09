a97d5350d5f361e2acf7ef40e0f059ae64c03739
Obi Faith <obifth@gmail.com>
2024-01-08 16:26:15 -0800

    Task 0 & 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - fills the first n bytes of the dest with src
+ * @src: memory location pointed at
+ * @dest: char to fill memory with
+ * @n: size of menory to fill b with
+ *
+ * Returns: a pointer to dest
+ */
+
+char *_memset(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i]
+
+	return (dest);
+}
