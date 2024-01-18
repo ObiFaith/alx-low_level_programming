554b41cc2bbcd81101ad7dc3ea23100ad3f96e6f
Obi Faith <obifth@gmail.com>
2024-01-18 14:36:45 -0800

    updated 1-strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,7 +11,7 @@ char *_strdup(char *str)
 {
 	char *result;
 
-	result = malloc((strlen(str) + 1) * size(char));
+	result = malloc((strlen(str) + 1) * sizeof(char));
 
 	if (result == NULL || str == NULL)
 		return (NULL);
