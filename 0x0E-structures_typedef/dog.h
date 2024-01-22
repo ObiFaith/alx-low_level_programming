78f810e5ecfb6e98e928187e94237f27ae586ef8
Obi Faith <obifth@gmail.com>
2024-01-22 14:28:08 -0800

    Modified dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,14 +13,14 @@
  * @owner: the owner of a dog
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
-struct dog dog_var;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
