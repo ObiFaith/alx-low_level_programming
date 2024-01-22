6b19c0dc71cc4b2db6f8fc3321d805574f24ae8f
Obi Faith <obifth@gmail.com>
2024-01-22 14:14:25 -0800

    Modified dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,14 +13,12 @@
  * Description: Struct name is dog &  struct var are created with its members
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
