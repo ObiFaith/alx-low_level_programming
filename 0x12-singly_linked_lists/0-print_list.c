81a8429a2345ba08e7cc1d21f4d85b4d2d33eaa0
Obi Faith <obifth@gmail.com>
2024-02-01 13:35:49 -0800

    Updated print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - a function that prints all the elements of a list_t list.
+ * @h: the head
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->str ? strlen(h->str) : 0, h->str ? h->str : "(nil)");
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
